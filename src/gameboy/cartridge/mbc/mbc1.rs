5b19ebb8e2aecf3e7538a4d6c249d94e1e1b25c1
Jonathan Como <jonathan.como@gmail.com>
2019-05-28 22:44:38 -0700

    tests

---
+++ src/gameboy/cartridge/mbc/mbc1.rs
@@ -0,0 +1,227 @@
+use super::MBC;
+
+#[derive(Debug, PartialEq)]
+enum BankingMode {
+    ROM,
+    RAM,
+}
+
+pub struct MBC1 {
+    rom_bank_lower_bits: u8,
+    rom_bank_upper_bits: u8,
+    ram_bank: u8,
+    ram_enabled: bool,
+    banking_mode: BankingMode,
+}
+
+impl MBC1 {
+    pub fn new() -> MBC1 {
+        MBC1 {
+            rom_bank_lower_bits: 1,
+            rom_bank_upper_bits: 0,
+            ram_bank: 0,
+            ram_enabled: false,
+            banking_mode: BankingMode::ROM,
+        }
+    }
+
+    fn get_rom_bank(&self) -> u16 {
+        let upper_bits = self.rom_bank_upper_bits & 0b11;
+        let lower_bits = self.rom_bank_lower_bits & 0x1F;
+        ((upper_bits << 5) | lower_bits) as u16
+    }
+
+    fn get_ram_bank(&self) -> u16 {
+        (self.ram_bank & 0b11) as u16
+    }
+}
+
+impl MBC for MBC1 {
+    fn read_rom_bank1(&self, rom: &[u8], address: u16) -> u8 {
+        let relative_address = address - 0x4000;
+        let index = relative_address + (0x4000 * self.get_rom_bank());
+        rom[index as usize]
+    }
+
+    fn read_ram(&self, ram: &[u8], address: u16) -> u8 {
+        if self.ram_enabled {
+            let index = address + (0x2000 * self.get_ram_bank());
+            ram[index as usize]
+        } else {
+            0xff
+        }
+    }
+
+    fn write_registers(&mut self, address: u16, byte: u8) {
+        match address >> 8 {
+            0x00...0x1F => {
+                self.ram_enabled = byte & 0x0F == 0x0A;
+            }
+            0x20...0x3F => {
+                self.rom_bank_lower_bits = match byte {
+                    0x00 => 0x01,
+                    0x20 => 0x21,
+                    0x40 => 0x41,
+                    0x60 => 0x61,
+                    _ => byte,
+                };
+            }
+            0x40...0x5F => match self.banking_mode {
+                BankingMode::ROM => self.rom_bank_upper_bits = byte,
+                BankingMode::RAM => self.ram_bank = byte,
+            },
+            0x60...0x7F => match byte & 0x01 {
+                0x00 => self.banking_mode = BankingMode::ROM,
+                0x01 => self.banking_mode = BankingMode::RAM,
+                _ => unreachable!(),
+            },
+            _ => unreachable!("Invalid register address: 0x{:x}", address),
+        }
+    }
+
+    fn write_ram(&mut self, ram: &mut [u8], address: u16, byte: u8) {
+        if self.ram_enabled {
+            let index = address + (0x2000 * self.get_ram_bank());
+            ram[index as usize] = byte;
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn get_ram_bank() {
+        let mut mbc = MBC1::new();
+
+        mbc.ram_bank = 0x01;
+        assert_eq!(0x01, mbc.get_ram_bank());
+
+        mbc.ram_bank = 0x0F;
+        assert_eq!(0x03, mbc.get_ram_bank());
+    }
+
+    #[test]
+    fn get_rom_bank() {
+        let mut mbc = MBC1::new();
+
+        mbc.rom_bank_lower_bits = 0x1F;
+        mbc.rom_bank_upper_bits = 0x03;
+
+        assert_eq!(0x7F, mbc.get_rom_bank());
+    }
+
+    #[test]
+    fn read_rom_bank1() {
+        let mut mbc = MBC1::new();
+        let mut rom = [0; 0xF000];
+
+        rom[0x4001] = 1;
+
+        assert_eq!(1, mbc.read_rom_bank1(&rom, 0x4001));
+
+        rom[0x8001] = 2;
+        mbc.rom_bank_lower_bits = 0x02;
+
+        assert_eq!(2, mbc.read_rom_bank1(&rom, 0x4001));
+    }
+
+    #[test]
+    fn read_ram() {
+        let mut mbc = MBC1::new();
+        let mut ram = [1; 0x8000];
+
+        mbc.ram_enabled = false;
+        assert_eq!(0xFF, mbc.read_ram(&ram, 0x40));
+
+        mbc.ram_enabled = true;
+        assert_eq!(1, mbc.read_ram(&ram, 0x40));
+    }
+
+    #[test]
+    fn write_registers_ram_enabled() {
+        let mut mbc = MBC1::new();
+
+        mbc.write_registers(0x00, 0xAA);
+        assert_eq!(true, mbc.ram_enabled);
+
+        mbc.write_registers(0x00, 0x0B);
+        assert_eq!(false, mbc.ram_enabled);
+
+        mbc.write_registers(0x00, 0x00);
+        assert_eq!(false, mbc.ram_enabled);
+    }
+
+    #[test]
+    fn write_registers_rom_bank() {
+        let mut mbc = MBC1::new();
+
+        mbc.write_registers(0x2000, 0x00);
+        assert_eq!(0x01, mbc.rom_bank_lower_bits);
+
+        mbc.write_registers(0x2000, 0x20);
+        assert_eq!(0x21, mbc.rom_bank_lower_bits);
+
+        mbc.write_registers(0x2000, 0x40);
+        assert_eq!(0x41, mbc.rom_bank_lower_bits);
+
+        mbc.write_registers(0x2000, 0x60);
+        assert_eq!(0x61, mbc.rom_bank_lower_bits);
+
+        mbc.write_registers(0x2000, 0x05);
+        assert_eq!(0x05, mbc.rom_bank_lower_bits);
+    }
+
+    #[test]
+    fn write_registers_rom_or_ram_bank() {
+        let mut mbc = MBC1::new();
+
+        mbc.banking_mode = BankingMode::ROM;
+        mbc.write_registers(0x4000, 0b11);
+
+        assert_eq!(0b11, mbc.rom_bank_upper_bits);
+        assert_eq!(0b00, mbc.ram_bank);
+
+        mbc.banking_mode = BankingMode::RAM;
+        mbc.write_registers(0x4000, 0b10);
+
+        assert_eq!(0b11, mbc.rom_bank_upper_bits);
+        assert_eq!(0b10, mbc.ram_bank);
+    }
+
+    #[test]
+    fn write_registers_banking_mode() {
+        let mut mbc = MBC1::new();
+
+        mbc.write_registers(0x6000, 0b11);
+        assert_eq!(BankingMode::RAM, mbc.banking_mode);
+
+        mbc.write_registers(0x6000, 0b01);
+        assert_eq!(BankingMode::RAM, mbc.banking_mode);
+
+        mbc.write_registers(0x6000, 0b00);
+        assert_eq!(BankingMode::ROM, mbc.banking_mode);
+    }
+
+    #[test]
+    fn write_ram() {
+        let mut mbc = MBC1::new();
+        let mut ram = [1; 0x8000];
+
+        mbc.ram_enabled = false;
+        mbc.write_ram(&mut ram, 0x1000, 0);
+
+        assert_eq!(1, ram[0x1000]);
+
+        mbc.ram_enabled = true;
+        mbc.write_ram(&mut ram, 0x1000, 0);
+
+        assert_eq!(0, ram[0x1000]);
+
+        mbc.ram_bank = 0b10;
+        mbc.write_ram(&mut ram, 0x1000, 5);
+
+        assert_eq!(5, ram[0x5000]);
+    }
+}
--- src/gameboy/cartridge/mod.rs
+++ src/gameboy/cartridge/mod.rs
@@ -1,117 +1,7 @@
-trait MBC {
-    fn read_rom_bank1(&self, rom: &[u8], address: u16) -> u8;
-    fn read_ram(&self, ram: &[u8], address: u16) -> u8;
-    fn write_registers(&mut self, rom: &[u8], address: u16, byte: u8);
-    fn write_ram(&mut self, ram: &mut [u8], address: u16, byte: u8);
-}
+mod mbc;
 
-struct NoMBC {}
-
-impl NoMBC {
-    fn new() -> NoMBC {
-        NoMBC {}
-    }
-}
-
-impl MBC for NoMBC {
-    fn read_rom_bank1(&self, rom: &[u8], address: u16) -> u8 {
-        rom[address as usize]
-    }
-
-    fn read_ram(&self, ram: &[u8], address: u16) -> u8 {
-        0xff
-    }
-
-    fn write_registers(&mut self, rom: &[u8], address: u16, byte: u8) {}
-
-    fn write_ram(&mut self, ram: &mut [u8], address: u16, byte: u8) {}
-}
-
-enum BankingMode {
-    ROM,
-    RAM,
-}
-
-struct MBC1 {
-    rom_bank_lower_bits: u8,
-    rom_bank_upper_bits: u8,
-    ram_bank: u8,
-    ram_enabled: bool,
-    banking_mode: BankingMode,
-}
-
-impl MBC1 {
-    fn new() -> MBC1 {
-        MBC1 {
-            rom_bank_lower_bits: 1,
-            rom_bank_upper_bits: 0,
-            ram_bank: 0,
-            ram_enabled: false,
-            banking_mode: BankingMode::ROM,
-        }
-    }
-
-    fn get_rom_bank(&self) -> u16 {
-        let upper_bits = self.rom_bank_upper_bits & 0b11;
-        let lower_bits = self.rom_bank_lower_bits & 0x1F;
-        ((upper_bits << 5) | lower_bits) as u16
-    }
-
-    fn get_ram_bank(&self) -> u16 {
-        (self.ram_bank & 0b11) as u16
-    }
-}
-
-impl MBC for MBC1 {
-    fn read_rom_bank1(&self, rom: &[u8], address: u16) -> u8 {
-        let relative_address = address - 0x4000;
-        let index = relative_address + (0x4000 * self.get_rom_bank());
-        rom[index as usize]
-    }
-
-    fn read_ram(&self, ram: &[u8], address: u16) -> u8 {
-        if self.ram_enabled {
-            let index = address + (0x2000 * self.get_ram_bank());
-            ram[index as usize]
-        } else {
-            0xff
-        }
-    }
-
-    fn write_registers(&mut self, rom: &[u8], address: u16, byte: u8) {
-        match address >> 8 {
-            0x00...0x1F => {
-                self.ram_enabled = byte & 0x0F == 0x0A;
-            }
-            0x20...0x3F => {
-                self.rom_bank_lower_bits = match byte {
-                    0x00 => 0x01,
-                    0x20 => 0x21,
-                    0x40 => 0x41,
-                    0x60 => 0x61,
-                    _ => byte,
-                };
-            }
-            0x40...0x5F => match self.banking_mode {
-                BankingMode::ROM => self.rom_bank_upper_bits = byte,
-                BankingMode::RAM => self.ram_bank = byte,
-            },
-            0x60...0x7F => match byte & 0x01 {
-                0x00 => self.banking_mode = BankingMode::ROM,
-                0x01 => self.banking_mode = BankingMode::RAM,
-                _ => unreachable!(),
-            },
-            _ => unreachable!("Invalid register address: 0x{:x}", address),
-        }
-    }
-
-    fn write_ram(&mut self, ram: &mut [u8], address: u16, byte: u8) {
-        if self.ram_enabled {
-            let index = address + (0x2000 * self.get_ram_bank());
-            ram[index as usize] = byte;
-        }
-    }
-}
+use self::mbc::mbc_from_byte;
+use self::mbc::MBC;
 
 pub struct Cartridge {
     mbc: Box<dyn MBC>,
@@ -122,8 +12,9 @@ pub struct Cartridge {
 impl Cartridge {
     pub fn new(data: Vec<u8>) -> Cartridge {
         let empty_ram = vec![0; 0x8000];
+
         Cartridge {
-            mbc: Cartridge::mbc_from_byte(data[0x147]),
+            mbc: mbc_from_byte(data[0x147]),
             rom: data.into_boxed_slice(),
             ram: empty_ram.into_boxed_slice(),
         }
@@ -138,7 +29,7 @@ impl Cartridge {
     }
 
     pub fn write_registers(&mut self, address: u16, byte: u8) {
-        self.mbc.write_registers(&self.rom, address, byte)
+        self.mbc.write_registers(address, byte)
     }
 
     pub fn read_ram(&self, address: u16) -> u8 {
@@ -147,13 +38,5 @@ impl Cartridge {
 
     pub fn write_ram(&mut self, address: u16, byte: u8) {
         self.mbc.write_ram(&mut self.ram, address, byte)
-    }
-
-    fn mbc_from_byte(byte: u8) -> Box<dyn MBC> {
-        match byte {
-            0x00 => Box::new(NoMBC::new()),
-            0x01...0x03 => Box::new(MBC1::new()),
-            _ => panic!("unimplemented MBC: 0x{:x}", byte),
-        }
     }
 }
